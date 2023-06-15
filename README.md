f36b9ef64c1a773573b7396888688f2ae0b7695c
CSWilder02 <126688647+CSWilder02@users.noreply.github.com>
2023-06-15 22:43:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 21100249_ClarisseStrijdom_DV200_T2_FinalProject
+ 
---
+++ doctors.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="patient.css">
+
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <script src="index.js"></script>
+
+
+</head>
+<body style="background-color: #F7FBFE; background-image: url('Images\Ellipse.png');">
+
+<nav class="navbar">
+<div class="navbar-logo">
+      <img src="Images\Logo.png" alt="Logo">
+    </div>
+    <div class="navbar-options">
+      <ul>
+      <li><a style="color: #126197" href="doctors.php">Doctors</a></li>
+        <li><a style="color: #126197" href="index.php">Appointments</a></li>
+        <li><a style="color: #126197" href="patients.php">Patients</a></li>
+        <li class="dropdown">
+          <a style="color: #126197" href="#">Profile</a>
+          <div class="dropdown-content">
+            <a href="#">Lock</a>
+            <a href="receptionistlogin.php">Logout</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  
+
+
+  <div class="container" style="background-image: url('Images/Cyrcledecor.png'); background-repeat: no-repeat;">
+  <h1 class="title_home">Upcoming Appointments</h1>
+
+  <div id="appointments" style="width: 650px; margin-left: auto; margin-right: auto; box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2); border-radius: 10px; padding-left: 80px; padding-top: 50px; margin-top: 50px">
+  <table class="table">
+    <tbody>
+      <?php include 'readdoctors.php'; ?>
+    </tbody>
+  </table>
+  
+  
+  <div id="table">
+    <form class="form-inline m-2" action="createpatient.php" method="POST">
+      <div style="margin-left: 40px" class="form-group">
+        <label for="name">Name & Surname:</label>
+        <input style="width: 600px" type="text" class="form-control m-2" id="name" name="patient_id">
+      </div> <break>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="score">Age:</label>
+        <input style="width: 600px;" type="number" class="form-control m-2" id="name" name="receptionist_id">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Gender: </label>
+        <input style="width: 600px" type="text" class="form-control m-2" id="name" name="doctor_id">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Email:</label>
+        <input style="width: 600px" type="text" class="form-control m-2" id="name" name="date">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Password:</label>
+        <input style="width: 600px" type="number" class="form-control m-2" id="name" name="room_id">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Phone Number:</label>
+        <input style="width: 600px" type="number" class="form-control m-2" id="name" name="room_id">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Specialisation:</label>
+        <input style="width: 600px" type="number" class="form-control m-2" id="name" name="room_id">
+      </div>
+      <div style="margin-left: 40px" class="form-group">
+        <label style="margin-top: 30px;" for="name">Room ID:</label>
+        <input style="width: 600px" type="number" class="form-control m-2" id="name" name="room_id">
+      </div>
+      <button type="submit" style="margin-top: 50px; margin-left: 40px; width: 300px; background-color: #09456B; padding-top: 10px; padding-bottom: 10px" class="btn btn-primary">Add</button>
+    </form>
+  </div>
+ 
+  
+
+
+
+</nav>
+</body>
+</html>
